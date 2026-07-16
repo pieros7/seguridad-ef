00bcee6e6ef6202265695372486a88aafc78d862
piero <piersss07@gmail.com>
2026-07-16 17:09:34 -0500

    chore: actualiza nombres de redes en script de limpieza

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker rm -f srv-web-cusco fw-cusco user1 router-central fw-lima srv-mysql-lima srv-ftp-lima cliente1-lima cliente2-lima 2>/dev/null || true
-docker network rm net_lan_cusco net_wan_cusco net_wan_lima net_dmz_lima net_lan_lima 2>/dev/null || true
+docker network rm net_lan_cusco net_wan_cusco net_wan_lima net_dmz_lima net_lan_lima net_cusco_lan net_cusco_wan net_lima_lan net_lima_wan 2>/dev/null || true
